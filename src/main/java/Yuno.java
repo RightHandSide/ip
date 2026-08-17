443f0fa9c48a7ef71226bdb4ef7972f94dfdef28
Lee Hong Sheng <e1525658@u.nus.edu>
2026-08-17 21:15:10 +0800

    Add startup greeting and farewell
    Display the Yuno banner and greeting when the application starts, then
    show a farewell message before the application exits.

--- src/main/java/Yuno.java
+++ src/main/java/Yuno.java
@@ -5,6 +5,12 @@ public class Yuno {
                 + " \\ V / | | | '_ \\ / _ \\\n"
                 + "  | | | |_| | | | | (_) |\n"
                 + "  |_|  \\__,_|_| |_|\\___/\n";
+        System.out.println("____________________________________________________________");
         System.out.println(banner);
+        System.out.println("Hello! I'm Yuno.\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
